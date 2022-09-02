998c1fe90d1d468ef0d205eacdf2f6233441fd14
Paulo Khayat <paulokhayat@gmail.com>
2022-09-03 01:22:27 +0300

    Add call and move inputs to elevator

--- ingame/Elevator.lua
+++ ingame/Elevator.lua
@@ -1,5 +1,12 @@
 upGearSide = "left"
 downGearSide = "right"
+
+topButtonMoveSide = "front"
+bottomButtonMoveSide = "back"
+
+topButtonCallSide = "bottom"
+bottomButtonCallSide = "top"
+
 function resetAllRedstone()
     for num, side in pairs(rs.getSides()) do
         rs.setOutput(side, false)
@@ -22,3 +29,39 @@ function moveElevatorDown()
     rs.setOutput(downGearSide, true)
     sleep(1) --tempFix from .1, see *1 at bottom
 end
+
+--
+--PERIODIC:
+--
+
+function periodic()
+    if rs.getInput(topButtonMoveSide) or
+           rs.getInput(bottomButtonCallSide) then
+
+        moveElevatorDown()
+
+    elseif rs.getInput(bottomButtonMoveSide) or
+            rs.getInput(topButtonCallSide) then
+
+        moveElevatorUp()
+    end
+end
+
+--
+--INIT:
+--
+
+resetAllRedstone()
+while true do
+    periodic()
+end
+
+--[[
+*1 if the input is 1sec the elevator will repeatedly "change directions"
+upwards only.
+I'd fix this by:
+    A) boolean/int for the current elevator direction
+    B) until released booleans for all 4 buttons...
+]]--
+
+    
