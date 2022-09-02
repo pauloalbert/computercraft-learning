9dd6a4b82a0a7c1d22aaaf00cfdd18757300304c
Paulo Khayat <paulokhayat@gmail.com>
2022-09-02 22:51:14 +0300

    create table filter for table keys

--- table.lua
+++ table.lua
@@ -29,3 +29,25 @@ function deepcopy(orig)
     end
     return copy
 end
+
+function filterTable(table, filter)
+    new_table = {}
+    for key, value in pairs(table) do
+        if filter(value) do
+            new_table[key] = value
+        end
+    end
+    return new_table
+end
+
+--[[
+local tempTable = TableClass(5,5,13)
+tempTable:set(1,1,0)
+local secondTable = TableClass.copy(tempTable)
+local thirdTable = deepcopy(tempTable)
+print(secondTable:get(1,1) .. " " .. thirdTable:get(1,1))
+tempTable:set(1,2,3)
+print(secondTable:get(1,2) .. " " .. thirdTable:get(1,2))
+print(tempTable:get(1,2))
+
+]]--
